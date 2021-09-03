6512f46f2c9f38140f41b3d47aef0fd68b216442
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2021-09-03 10:09:52 -0500

    Consultando todas las películas y series

--- src/helpers/fetchData.js
+++ src/helpers/fetchData.js
@@ -2,11 +2,29 @@ import axios from 'axios';
 import { GET_MOVIES, GET_SERIES } from '../fetch/urls';
 
 export const getAllMovies = async () => {
+    let allMovies = [];
     const resp = await axios.get(GET_MOVIES);
-    return await resp.data.results;
+    const total_pages = await resp.data.total_pages;
+    for (let i = 1; i <= total_pages; i++) {
+        const response = await axios.get(GET_MOVIES + `&page=${i}`);
+        const movies = await response.data.results;
+        for (const movie of movies) {
+            allMovies.push(movie);
+        }
+    }
+    return allMovies;
 }
 
 export const getAllSeries = async () => {
+    let allSeries = [];
     const resp = await axios.get(GET_SERIES);
-    return await resp.data.results;
+    const total_pages = await resp.data.total_pages;
+    for (let i = 1; i <= total_pages; i++) {
+        const response = await axios.get(GET_SERIES + `&page=${i}`);
+        const series = await response.data.results;
+        for (const serie of series) {
+            allSeries.push(serie);
+        }
+    }
+    return allSeries;
 }
