0b4190595ab369660f7ddb2fc33b46509a573eb2
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2021-09-03 10:06:53 -0500

    Ícono agregado y géneros y opciones de favorito optimizados

--- src/components/SerieItem.js
+++ src/components/SerieItem.js
@@ -1,44 +1,56 @@
 import React, { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
 import Swal from 'sweetalert2';
 import favorite from '../assets/favorite.png';
-
+import noPoster from '../assets/no-poster.jpeg';
 import { getGenres } from '../helpers/genres';
+import { addFavorite } from '../redux/actions/favorites';
 
 const SerieItem = ({ id, poster_path, name, overview, vote_average, genre_ids, first_air_date, video }) => {
+
+    const dispatch = useDispatch();
+
     const [genres, setGenres] = useState([]);
 
     useEffect(() => {
-        getGenres(genre_ids)
-            .then(data => setGenres(data));
-    }, []);
+        settingGenres();
+    }, [genre_ids]);
+
+    const settingGenres = async () => {
+        const resp = await getGenres(genre_ids);
+        setGenres(resp);
+    }
 
     const isVideo = () => {
         if (!video) {
-            Swal.fire('Lo sentimos', 'No hay tráiler para esta película', 'warning');
+            Swal.fire('Lo sentimos', 'No hay tráiler para esta serie', 'warning');
         }
     }
 
-    const addFavorite = () => {
+    const addingFavorite = () => {
         const itemSeries = { id, poster_path, name, overview, vote_average, genre_ids, first_air_date };
-
-        window.$favorites = [...window.$favorites, itemSeries];
-
-        Swal.fire('Éxito', 'Película agregada a favoritos', 'success');
+        dispatch(addFavorite(itemSeries));
+        Swal.fire('Éxito', 'Serie agregada a favoritos', 'success');
     }
 
     return (
         <div className="container-card">
             <div className="card">
-                <img src={"https://image.tmdb.org/t/p/w500" + poster_path} alt={name} />
+                {poster_path
+                    ?
+                    <img src={"https://image.tmdb.org/t/p/w500" + poster_path} alt={name} />
+                    :
+                    <img src={noPoster} alt={name} />
+                }
                 <h5 className="card-title">{name}</h5>
                 <h5 className="card-subtitle">{vote_average}</h5>
                 <p className="card-text-release">{first_air_date}</p>
                 {(genres === "")
-                ?
-                <p className="card-text-genres">{"Sin género"}</p>
-                :
-                <p className="card-text-genres">{genres}</p>
-            }
+                    ?
+                    <p className="card-text-genres">{"Sin género"}</p>
+                    :
+                    <p className="card-text-genres">{genres}</p>
+                }
                 {(overview === "")
                     ?
                     <p className="card-text-overview">{"Sin descripción"}</p>
@@ -53,7 +65,7 @@ const SerieItem = ({ id, poster_path, name, overview, vote_average, genre_ids, f
                 </button>
                 <button
                     className="button-favorite"
-                    onClick={addFavorite}
+                    onClick={addingFavorite}
                 >
                     Agregar a favoritos
                     <img src={favorite} alt="favorite" className="favIcon" />
