01fa117a8bec8054686193ecf28fd4248a9c65be
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2021-09-03 10:11:34 -0500

    Eliminación de favoritos

--- src/types/types.js
+++ src/types/types.js
@@ -4,6 +4,7 @@ export const types = {
     seriesRead: '[Series] Read series',
     favoritesRead: '[Favorites] Read favorites',
     favoritesAdd: '[Favorites] Add favorites',
+    favoritesDelete: '[Favorites] Delete favorites',
     gendersRead: '[Favorites] Read genders',
     moviesYearsRead: '[Years] Read Movies years',
     seriesYearsRead: '[Years] Read Series years'
