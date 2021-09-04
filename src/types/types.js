cc3985a9b5dbf84bc3630fe924e935b66a30d4b6
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2021-09-04 02:04:49 -0500

    Operaciones agregadas sobre genres

--- src/types/types.js
+++ src/types/types.js
@@ -5,7 +5,10 @@ export const types = {
     favoritesRead: '[Favorites] Read favorites',
     favoritesAdd: '[Favorites] Add favorites',
     favoritesDelete: '[Favorites] Delete favorites',
-    gendersRead: '[Favorites] Read genders',
+    moviesGenresRead: '[Movies] Read movies genres',
+    seriesGenresRead: '[Series] Read series genres',
+    addGenre: '[All] Add genre',
+    allGenresRead: '[All] Read all genres',
     moviesYearsRead: '[Years] Read Movies years',
     seriesYearsRead: '[Years] Read Series years'
 }
