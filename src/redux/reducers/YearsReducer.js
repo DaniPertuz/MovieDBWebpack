8bb910d082e04d691187ebafd034afc09c6572e6
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2021-09-04 14:18:30 -0500

    Nuevas acciones sobre years agregadas en reducer

--- src/redux/reducers/YearsReducer.js
+++ src/redux/reducers/YearsReducer.js
@@ -1,22 +1,34 @@
 import { types } from '../../types/types';
 
-const initialState = { moviesYears: [], seriesYears: [] };
+const initialState = { moviesYears: [], seriesYears: [], allYears: [] };
 
 export default (state = initialState, { type, payload }) => {
     switch (type) {
+        case types.moviesYearsRead:
+            return {
+                ...state,
+                moviesYears: [...payload]
+            }
+        case types.seriesYearsRead:
+            return {
+                ...state,
+                seriesYears: [...payload]
+            }
 
-    case types.moviesYearsRead:
-        return {
-            ...state,
-            moviesYears: [...payload]
-        }
-    case types.seriesYearsRead:
-        return {
-            ...state,
-            seriesYears: [...payload]
+        case types.allYearsRead:
+            return {
+                ...state,
+                allYears: [...payload]
+            }
+
+        case types.addYear: {
+            return {
+                ...state,
+                allYears: [...state.allYears, payload]
+            }
         }
 
-    default:
-        return state;
+        default:
+            return state;
     }
 }
