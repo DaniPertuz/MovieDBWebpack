da6e956e3f358846e7e19080a900b0069f75f0fd
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2021-09-04 02:06:28 -0500

    Leer géneros por movie y por serie

--- src/helpers/genres.js
+++ src/helpers/genres.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
-import { GET_GENRES } from '../fetch/urls';
+import { GET_GENRES_MOVIES, GET_GENRES_SERIES } from '../fetch/urls';
 
-export const getGenres = async (genres = []) => {
-    const response = await axios.get(GET_GENRES);
+export const getGenresMovies = async (genres = []) => {
+    const response = await axios.get(GET_GENRES_MOVIES);
     const body = await response.data.genres;
     let genders = [];
 
@@ -19,8 +19,62 @@ export const getGenres = async (genres = []) => {
     return genders.join(', ');
 }
 
-export const genresList = async () => {
-    const response = await axios.get(GET_GENRES);
+export const getGenresSeries = async (genres = []) => {
+    const response = await axios.get(GET_GENRES_SERIES);
+    const body = await response.data.genres;
+    let genders = [];
 
-    return await response.data.genres;
+    for (let i = 0; i < genres.length; i++) {
+        const id = genres[i];
+        for (let j = 0; j < body.length; j++) {
+            const idGender = body[j].id;
+            if (id === idGender) {
+                genders.push(body[j].name);
+            }
+        }
+    }
+
+    return genders.join(', ');
+}
+
+export const genresList = async (allGenres = []) => {
+    allGenres.sort();
+    const uniqueGenres = allGenres.filter((v, i, a) => a.indexOf(v) === i);
+
+    const response = await axios.get(GET_GENRES_MOVIES);
+    const movies = await response.data.genres;
+
+    const resp = await axios.get(GET_GENRES_SERIES);
+    const series = await resp.data.genres;
+
+    let genders = [], allGenresList = [];
+
+    for (let i = 0; i < uniqueGenres.length; i++) {
+        const item = uniqueGenres[i];
+        for (let i = 0; i < item.length; i++) {
+            const element = item[i];
+            allGenresList.push(element);
+        }
+    }
+
+    for (const genre of allGenresList) {
+        for (const movie of movies) {
+            const idGender = movie.id;
+            if (genre === idGender) {
+                genders.push(movie.name);
+            }
+        }
+        for (let k = 0; k < series.length; k++) {
+            const idGender = series[k].id;
+            if (genre === idGender) {
+                genders.push(series[k].name);
+            }
+        }
+    }
+
+    genders.sort();
+
+    const uniques = genders.filter((v, i, a) => a.indexOf(v) === i);
+
+    return uniques;
 }
--- src/redux/actions/genders.js
+++ src/redux/actions/genders.js
@@ -1,22 +1,73 @@
 import axios from 'axios';
-import { GET_GENRES } from '../../fetch/urls';
-import { getGenres } from '../../helpers/genres';
+import { GET_GENRES_MOVIES, GET_GENRES_SERIES } from '../../fetch/urls';
+import { genresList } from '../../helpers/genres';
 import { types } from '../../types/types';
 
-export const getGenders = () => {
+export const getGendersMovies = () => {
     return async (dispatch) => {
         try {
-            const resp = await axios.get(GET_GENRES);
+            const resp = await axios.get(GET_GENRES_MOVIES);
             const genders = await resp.data.genres;
 
-            dispatch(loadingGenders(genders));
+            dispatch(loadingGendersMovies(genders));
         } catch (error) {
             console.log(error);
         }
     }
 }
 
-const loadingGenders = (genders) => ({
-    type: types.gendersRead,
+const loadingGendersMovies = (genders) => ({
+    type: types.moviesGenresRead,
+    payload: genders
+});
+
+export const getGendersSeries = () => {
+    return async (dispatch) => {
+        try {
+            const resp = await axios.get(GET_GENRES_SERIES);
+            const genders = await resp.data.genres;
+
+            dispatch(loadingGendersSeries(genders));
+        } catch (error) {
+            console.log(error);
+        }
+    }
+}
+
+const loadingGendersSeries = (genders) => ({
+    type: types.seriesGenresRead,
+    payload: genders
+});
+
+export const addGenres = (genres) => {
+    return async (dispatch) => {
+        try {
+            dispatch(addingGenres(genres));
+        } catch (error) {
+            console.log(error);
+        }
+    }
+}
+
+const addingGenres = (genres) => ({
+    type: types.addGenre,
+    payload: genres
+});
+
+export const getAllGenres = () => {
+    return async (dispatch, getState) => {
+        const { genders } = getState();
+        try {
+            const genres = await genresList(genders.allGenres);
+
+            dispatch(loadingAllGenders(genres));
+        } catch (error) {
+            console.log(error);
+        }
+    }
+}
+
+const loadingAllGenders = (genders) => ({
+    type: types.allGenresRead,
     payload: genders
 });
