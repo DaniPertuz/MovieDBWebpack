b39a63cd50141692a96720cc72f80faaf0355db6
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2021-09-07 11:55:39 -0500

    Acciones en localStorage

--- src/redux/actions/favorites.js
+++ src/redux/actions/favorites.js
@@ -1,29 +1,21 @@
 import { types } from '../../types/types';
 
 export const getFavorites = () => {
-    return async (dispatch, getState) => {
+    return async () => {
         try {
-            const { favorites } = getState();
-            dispatch(loadingFavorites(favorites.data));
+            const localFavorites = JSON.parse(localStorage.getItem('favorites'));
+            localStorage.setItem('favorites', JSON.stringify(localFavorites));
         } catch (error) {
             console.log(error);
         }
     }
 }
 
-const loadingFavorites = (favorites) => ({
-    type: types.favoritesRead,
-    payload: favorites
-});
-
 export const addFavorite = (favorite) => {
-    return async (dispatch) => {
+    return async () => {
         try {
-            // dispatch(favoriteAddNew(favorite));
             const localFavorites = JSON.parse(localStorage.getItem('favorites'));
-            console.log(localFavorites);
             const addingLocalFavorite = [...localFavorites, favorite];
-            console.log(addingLocalFavorite);
             localStorage.setItem('favorites', JSON.stringify(addingLocalFavorite));
         } catch (error) {
             console.error(error);
@@ -31,26 +23,14 @@ export const addFavorite = (favorite) => {
     }
 }
 
-const favoriteAddNew = (favorite) => ({
-    type: types.favoritesAdd,
-    payload: favorite
-});
-
 export const deleteFavorite = (id) => {
-    return async (dispatch) => {
+    return async () => {
         try {
-            dispatch(favoriteDeleting(id));
             const localFavoritesToDelete = JSON.parse(localStorage.getItem('favorites'));
             const deleted = localFavoritesToDelete.filter(fav => fav.id !== id);
-            console.log(typeof deleted);
             localStorage.setItem('favorites', JSON.stringify(deleted));
         } catch (error) {
             console.error(error);
         }
     }
 }
-
-const favoriteDeleting = (id) => ({
-    type: types.favoritesDelete,
-    payload: id
-});
